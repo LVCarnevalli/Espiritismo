f5963dc6d46d2f13447430efbd0b8443fb298b03
Carnevalli <verdi@hotmail.com.br>
2020-02-09 10:46:06 -0300

    Deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -7,4 +7,5 @@ echo $GOOGLE_ANALYTICS_TRACKING_ID >> .env
 
 yarn global add expo-cli@3.11.9
 expo login -u $EXPO_USERNAME -p $EXPO_PASSWORD --non-interactive
-expo build:android
+#expo build:android
+expo publish
